47e9fdaec425fbcfe7acd22a1bad2ea599f91bb7
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2025-04-01 01:15:32 +0700

    Initial

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,24 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $users = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+            ],
+            [
+                'name' => 'User',
+                'email' => 'user@admin.com',
+            ]
+        ];
+        foreach ($users as $user) {
+            User::factory()->create($user);
+        }
     }
 }
